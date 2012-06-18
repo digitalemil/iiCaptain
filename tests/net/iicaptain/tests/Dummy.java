52844996086d4e59826a2a081958f5e8eefbd255
digitalemil <digitalemil@googlemail.com>
2012-06-18 10:47:04 +0200

    Added folder tests

---
+++ tests/net/iicaptain/tests/Dummy.java
@@ -0,0 +1,14 @@
+package net.iicaptain.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Dummy {
+
+	@Test
+	public void test() {
+		assertEquals("pass", "pass");
+	}
+
+}
